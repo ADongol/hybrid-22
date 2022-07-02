4863b489370b2f47d3834f1c58332fccea8df591
ANAND DONGOL <anandongol@hotmail.com>
2022-07-02 15:34:32 -0500

    initial commit

---
+++ dotnetcore-with-blazor/ng-core-101/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace ng_core_101;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
